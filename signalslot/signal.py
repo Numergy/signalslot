ef8dc68fa39e655f987b398f8b6e8656a3cde0ba
jpic <jamespic@gmail.com>
2014-03-14 15:08:58 +0100

    Implemented Signal.is_compatible() to check if a slot has a compatible signature for a signal

--- signalslot/signal.py
+++ signalslot/signal.py
@@ -2,8 +2,9 @@ from . import exceptions
 
 
 class Signal(object):
-    def __init__(self):
+    def __init__(self, args=None):
         self.slots = []
+        self.args = args or []
 
     def connect(self, slot):
         """ Connect a slot to this signal. """
@@ -20,3 +21,8 @@ class Signal(object):
         """ Call all slots connected to this signal. """
         for slot in self.slots:
             slot()
+
+    def is_compatible(self, slot):
+        """ Return True if slot is compatible with args of this signal. """
+        args = slot.func_code.co_varnames[:slot.func_code.co_argcount]
+        return list(args) == list(self.args)
--- signalslot/tests.py
+++ signalslot/tests.py
@@ -4,7 +4,7 @@ import unittest
 import signalslot
 
 
-class SignalSlotTestCase(unittest.TestCase):
+class NoArgumentsSignalTestCase(unittest.TestCase):
     def setUp(self):
         self.slot = mock.MagicMock()
         self.signal = signalslot.Signal()
@@ -29,3 +29,45 @@ class SignalSlotTestCase(unittest.TestCase):
         self.signal.emit()
 
         self.slot.assert_called_once_with()
+
+
+class SignalIsCompatibleTestCases(unittest.TestCase):
+    def test_with_one_argument(self):
+        signal = signalslot.Signal(args=['firewall'])
+
+        def test_slot(firewall):
+            pass
+
+        self.assertTrue(signal.is_compatible(test_slot))
+
+    def test_with_one_argument_with_keyword(self):
+        signal = signalslot.Signal(args=['firewall'])
+
+        def test_slot(firewall=None):
+            pass
+
+        self.assertTrue(signal.is_compatible(test_slot))
+
+    def test_with_asterisk_args(self):
+        signal = signalslot.Signal(args=['firewall'])
+
+        def test_slot(*args):
+            pass
+
+        self.assertFalse(signal.is_compatible(test_slot))
+
+    def test_with_asterisk_args(self):
+        signal = signalslot.Signal(args=['firewall'])
+
+        def test_slot(**kwargs):
+            pass
+
+        self.assertFalse(signal.is_compatible(test_slot))
+
+    def test_connect_wrong_slot_signature(self):
+        signal = signalslot.Signal(args=['firewall'])
+
+        def test_slot():
+            pass
+
+        self.assertFalse(signal.is_compatible(test_slot))
