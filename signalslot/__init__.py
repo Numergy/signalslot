2d07baab0578ea40d2e35a74338558535ddc5b8f
Stuart Longland <me@vk4msl.id.au>
2016-01-26 06:09:53 +1000

    Fix PEP8 violation in # pragma: no cover

--- signalslot/__init__.py
+++ signalslot/__init__.py
@@ -2,7 +2,7 @@ try:
     from .signal import Signal
     from .slot import Slot
     from .exceptions import *
-except ImportError: # pragma: no cover
+except ImportError:  # pragma: no cover
     # Possible we are running from setup.py, in which case we're after
     # the __version__ string only.
     pass
