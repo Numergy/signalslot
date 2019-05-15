aef89e9535689d06153a7c665772afef8704643b
Sliim <sliim@mailoo.org>
2019-05-15 12:56:58 +0200

    Clarify licensing (MIT)
    issue #15

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
